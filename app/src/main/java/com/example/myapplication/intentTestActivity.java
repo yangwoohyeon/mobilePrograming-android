f00cbf6e66eb2428685b311cd1a68a1009e1f709
yangwoohyeon <dngus0717@gmail.com>
2024-12-03 23:19:20 +0900

    뷰 인텐트 실습

---
+++ app/src/main/java/com/example/myapplication/intentTestActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class intentTestActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+
+
+    public void onCreate(){
+
+    }
+}
