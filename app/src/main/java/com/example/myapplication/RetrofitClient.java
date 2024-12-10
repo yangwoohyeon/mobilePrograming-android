442a53e4e8f9243bbc8e4d09c2154e15cf51710f
yangwoohyeon <dngus0717@gmail.com>
2024-12-10 22:30:38 +0900

    실습 복습

--- app/src/main/java/com/example/myapplication/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/RetrofitClient.java
@@ -10,7 +10,7 @@ public class RetrofitClient {
         if(retrofit==null){
             Retrofit.Builder builder = new Retrofit.Builder();
             builder.baseUrl("http://10.255.89.160:8080/");
-            builder.addConverterFactory(GsonConverterFactory.create()); //new 쓰면 안됨.
+            builder.addConverterFactory(GsonConverterFactory.create());
             retrofit=builder.build();
         }
 
